3ca5dec8a1bdc3a9821e2f583c13019467555a2a
Henrique Pires Ouriques <henriquepiresouriques@gmail.com>
2022-01-17 21:22:53 -0300

    more styles and cards create
    adicionado mais alguns estilos para os cards já existentes e a criação de novos

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
 import { HighlightCard } from '../../components/HighlightCard';
+import { TransactionCard } from '../../components/TransactionCard';
 
 import { 
     Container, 
@@ -11,10 +13,23 @@ import {
     UserGreeting,
     UserName,
     Icon,
-    HighlightCards
+    HighlightCards,
+    Transactions,
+    Title,
+    TransactionList
 } from './styles';
 
 export function Dashboard() {
+    const data =[{
+        title: "Desenvolvimento de site",
+        amount: "R$ 12.000,00",
+        category: {
+            name: 'Vendas',
+            icon: 'dollar-sign'
+        },
+        date: "13\04\2022",
+    }];
+
     return (
         <Container>
             <Header>
@@ -34,10 +49,40 @@ export function Dashboard() {
             </Header>
 
             <HighlightCards>
-                <HighlightCard />
-                <HighlightCard />
-                <HighlightCard />
+                <HighlightCard 
+                    type='up'
+                    title='Entradas'
+                    amount='R$ 17.400,00'
+                    lastTransaction='Última entrada dia 13 de abril'
+                />
+                <HighlightCard 
+                    type='down'
+                    title='Saídas'
+                    amount='R$ 1.259,00'
+                    lastTransaction='Última saída dia 3 de abril'
+                />
+                <HighlightCard 
+                    type='total'
+                    title='Total'
+                    amount='R$ 16.141,00'
+                    lastTransaction='Última 01 à 16 de abril'
+                />
             </HighlightCards>
+
+            <Transactions>
+                <Title>Listagem</Title>
+
+                <TransactionList 
+                    data={data}
+                    renderItem={({ item }) => <TransactionCard data={item} />}
+                    showsVerticalScrollIndicator={false}
+                    contentContainerStyle={{
+                        paddingBottom: getBottomSpace()
+                    }}
+                />
+
+            </Transactions>
+
         </Container>
     )
-}
+} 
